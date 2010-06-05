ca639fb1e8590e78f15f22d528b9d9cb8a3035d3
Andreas Kollegger <akollegger@tembopublic.org>
2010-06-05 19:50:02 -0400

    added bnd4sbt to produce an OSGi friendly jar

--- project/build/ConfiggyProject.scala
+++ project/build/ConfiggyProject.scala
@@ -1,10 +1,11 @@
 import sbt._
 import com.twitter.sbt._
+import com.weiglewilczek.bnd4sbt._
 
-
-class ConfiggyProject(info: ProjectInfo) extends StandardProject(info) {
+class ConfiggyProject(info: ProjectInfo) extends DefaultProject(info) with BNDPlugin {
   val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1"
-  val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3"
+  //val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3"
+  val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1"
 
   override def pomExtra =
     <licenses>
@@ -14,6 +15,11 @@ class ConfiggyProject(info: ProjectInfo) extends StandardProject(info) {
         <distribution>repo</distribution>
       </license>
     </licenses>
+
+  // osgi bundle specifications
+  override def bndExportPackage = Set("net.lag.logging",
+    "net.lag",
+    "net.lag.configgy")
 
   Credentials(Path.userHome / ".ivy2" / "credentials", log)
   val publishTo = "nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -4,4 +4,5 @@ import sbt._
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val twitterNest = "com.twitter" at "http://www.lag.net/nest"
   val defaultProject = "com.twitter" % "standard-project" % "0.5.5"
+  val bnd4sbt = "com.weiglewilczek" % "bnd4sbt" % "0.4"
 }
